47e6252735d4b5dfdacd871134fdcbabae7de322
Njoud Aldarbas <>
2024-05-09 10:47:01 +0300

    simple CRUD

---
+++ pages/create.vue
@@ -0,0 +1,110 @@
+<template>
+  <form @submit.prevent="saveProducte" class="max-w-sm mx-auto">
+    <div class="mb-5">
+      <label
+        for="large-input"
+        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >PRODUCT NAME</label
+      >
+      <input
+        v-model="producte.title"
+        type="text"
+        id="large-input"
+        class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      />
+    </div>
+    <div class="mb-5">
+      <label
+        for="message"
+        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >DESCRIPTION</label
+      >
+      <textarea
+        v-model="producte.description"
+        id="message"
+        rows="4"
+        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="Write a Good Descripton ..."
+      ></textarea>
+    </div>
+    <div class="mb-5">
+      <label
+        for="base-input"
+        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >CATEGORY</label
+      >
+      <input
+        v-model="producte.category"
+        type="text"
+        id="base-input"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      />
+    </div>
+    <div>
+      <label
+        for="small-input"
+        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >PRICE</label
+      >
+      <input
+        v-model="producte.price"
+        type="text"
+        id="small-input"
+        class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      />
+    </div>
+
+    <div class="mt-6 flex items-center justify-end gap-x-6">
+      <NuxtLink
+        to="/"
+        class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+      >
+        <span
+          class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0"
+        >
+          Cancel
+        </span>
+      </NuxtLink>
+
+      <button
+        class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+      >
+        <span
+          class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0"
+        >
+          Save
+        </span>
+      </button>
+    </div>
+  </form>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      producte: {
+        title: "",
+        description: "",
+        category: "",
+        price: "",
+      },
+    };
+  },
+  methods: {
+    async saveProducte() {
+      axios
+        .post(`http://localhost:3000/products`, this.producte)
+        .then((response) => {
+          // useState(response.data.data);
+          console.log(response);
+          
+        });
+        await navigateTo({ path: '/' })
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,130 @@
+<script setup lang="ts">
+// import axios from "axios";
+useHead({
+  title: "Dashboard",
+});
+//Get Function
+const { data: products,refresh } = await useFetch<any>(
+  ' http://localhost:3000/products'
+);
+console.log(products);
+refresh()
+//Delete function
+const remove = async (id: number) => {
+  fetch(`http://localhost:3000/products/${id}`, {
+    method: "DELETE",
+  })
+    .then((res) => res.json())
+    .then(console.log);
+    alert(id + " delete successfully")
+    refresh()
+};
+
+</script>
+
+<template>
+  <div class="md-2 shadow-md sm:rounded-lg">
+    <NuxtLink
+      to="/create"
+      class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+    >
+      <span
+        class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0"
+      >
+        Add Product
+      </span>
+    </NuxtLink>
+
+    <table
+      class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
+    >
+      <thead
+        class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+      >
+        <tr>
+          <th scope="col" class="px-6 py-3">Product name</th>
+          <th scope="col" class="px-6 py-3">description</th>
+          <th scope="col" class="px-6 py-3">Category</th>
+          <th scope="col" class="px-6 py-3">Price</th>
+          <th scope="col" class="px-6 py-3">
+            <span>Action</span>
+          </th>
+          <th scope="col" class="px-6 py-3">
+            <span class="sr-only">Action</span>
+          </th>
+        </tr>
+      </thead>
+      <tbody v-for="(p, index) in products" v-bind:key="index">
+        <tr
+          class="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600"
+        >
+          <th
+            scope="row"
+            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+          >
+            {{ p.title }}
+          </th>
+          <td class="px-6 py-4">
+            {{ p.description }}
+          </td>
+          <td class="px-6 py-4">
+            {{ p.category }}
+          </td>
+          <td class="px-6 py-4">
+            {{ p.price }}
+          </td>
+          <td class="px-6 py-4 text-right">
+            <NuxtLink
+              :to="`/${p.id}`"
+              class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+            >
+              <span
+                class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0"
+              >
+                Edit
+              </span>
+              <svg
+                class="fill-white"
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="20"
+                height="20"
+                viewBox="0 0 50 50"
+              >
+                <path
+                  d="M 46.574219 3.425781 C 45.625 2.476563 44.378906 2 43.132813 2 C 41.886719 2 40.640625 2.476563 39.691406 3.425781 C 39.691406 3.425781 39.621094 3.492188 39.53125 3.585938 C 39.523438 3.59375 39.511719 3.597656 39.503906 3.605469 L 4.300781 38.804688 C 4.179688 38.929688 4.089844 39.082031 4.042969 39.253906 L 2.035156 46.742188 C 1.941406 47.085938 2.039063 47.453125 2.292969 47.707031 C 2.484375 47.898438 2.738281 48 3 48 C 3.085938 48 3.171875 47.988281 3.257813 47.964844 L 10.746094 45.957031 C 10.917969 45.910156 11.070313 45.820313 11.195313 45.695313 L 46.394531 10.5 C 46.40625 10.488281 46.410156 10.472656 46.417969 10.460938 C 46.507813 10.371094 46.570313 10.308594 46.570313 10.308594 C 48.476563 8.40625 48.476563 5.324219 46.574219 3.425781 Z M 45.160156 4.839844 C 46.277344 5.957031 46.277344 7.777344 45.160156 8.894531 C 44.828125 9.222656 44.546875 9.507813 44.304688 9.75 L 40.25 5.695313 C 40.710938 5.234375 41.105469 4.839844 41.105469 4.839844 C 41.644531 4.296875 42.367188 4 43.132813 4 C 43.898438 4 44.617188 4.300781 45.160156 4.839844 Z M 5.605469 41.152344 L 8.847656 44.394531 L 4.414063 45.585938 Z"
+                ></path>
+              </svg>
+            </NuxtLink>
+          </td>
+          <td class="px-6 py-4 text-right">
+            <NuxtLink
+              @click="remove(p.id)"
+              class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+            >
+              <span
+                class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0"
+              >
+                Delete
+              </span>
+              <svg
+                class="fill-white"
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M 10 2 L 9 3 L 3 3 L 3 5 L 21 5 L 21 3 L 15 3 L 14 2 L 10 2 z M 4.3652344 7 L 5.8925781 20.263672 C 6.0245781 21.253672 6.877 22 7.875 22 L 16.123047 22 C 17.121047 22 17.974422 21.254859 18.107422 20.255859 L 19.634766 7 L 4.3652344 7 z"
+                ></path>
+              </svg>
+            </NuxtLink>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
